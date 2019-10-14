ae25c3e9be48ab537c6d0431a7a2af28c2277b2e
david <lucht013@morris.umn.edu>
2019-10-14 12:16:20 -0500

    Update machine object to align with api
    This adds the position and name values for the machine object that the
    api returns when called.

--- client/src/app/machines/machine.ts
+++ client/src/app/machines/machine.ts
@@ -1,7 +1,12 @@
 export interface Machine {
   _id: string;
+  name: string;
   type: string; //'washer','dryer'
   running: boolean;
   status: string; //Can be 'normal', 'broken', or 'invisible'
+  position: {
+    x: number;
+    y: number;
+  };
   room_id: string; //'gay_hall','independence_hall','spooner','blakely','green_prairie'
 }
